cf95fa73e545ba3bf5daa89329645a1060743f05
Klein Simon <klein.simon@csedge.org>
2018-10-06 15:46:36 -0400

    Completed Challenge 1

--- data.js
+++ data.js
@@ -1,5 +1,29 @@
-// Example
+// E
 let message = "Hello World!";
 console.log(message);
-
-// Type your code below!
+//
+let f
+let k;
+k=10;
+let a;
+a=20;
+let s;
+s=30;
+let q='I love apples';
+let w='Im learning french';
+let e='I just ate pizza'; 
+loveSoccer=true;
+likesRaisins=false;
+console.log(k);
+console.log(a);
+console.log(s);
+console.log(q);
+console.log(w);
+console.log(e);
+console.log(loveSoccer);
+console.log(likesRaisins);
+console.log(f);
+console.log(typeof loveSoccer);
+const READLINE=require('readline-sync');
+let userInput=READLINE.question('Type anything and Ill repeat it back!');
+console.log('You typed:${userInput}');
